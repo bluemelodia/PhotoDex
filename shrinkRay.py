71b4b99fed90264e262080b15c96652b2a41cdf9
bluemelodia <hsumelanie@yahoo.com>
2015-08-02 15:31:22 -0400

    Shrink ray victims come out in the right proportions.

--- shrinkRay.py
+++ shrinkRay.py
@@ -1,3 +1,5 @@
+# A handy ray that will shrink the size of your images
+
 import cv2
 
 def shrink(image, width):
@@ -5,14 +7,15 @@ def shrink(image, width):
 
 	# preserve aspect ratio so the image does not look skewed or distorted 
 	ratio = img.shape[0]/float(img.shape[1])
-	dimension = (width, (width*img.shape[1])/img.shape[0])
+	dimension = (width, (width*img.shape[0])/img.shape[1])
 	print img.shape
 	print ratio
 	print dimension
 
 	# resize the image
 	resizedImg = cv2.resize(img, dimension, interpolation = cv2.INTER_AREA)
-	cv2.imshow("resized", resizedImg)
-	cv2.waitKey(0)
-
 	
+	# overwrite the old image
+	cv2.imwrite(image, resizedImg)
+
+
