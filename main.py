178e25b41dfc694f0d937a9447c9e302dc83d30e
bluemelodia <hsumelanie@yahoo.com>
2015-08-09 17:14:21 -0400

    Added a new protocol, D, and cleaned up system error messages.

--- main.py
+++ main.py
@@ -6,10 +6,12 @@
 import cv2
 import os
 import sys
+import imghdr
 
 import faceDetection
 import shrinkRay
 import colorCode
+import cloneCrush
 
 def main():
 	print "\n- - - - - Welcome to PhotoDex Laboratory - - - - -\n"
@@ -18,15 +20,14 @@ def main():
 	try:
 		HomeDirectory = os.listdir(Home)
 	except:
-		print "\nA giant tortoise is using your home as her new shell! Please provide the address of an abode you still own...\n"
-		sys.exit()
+		sys.exit("\nA giant tortoise is using your home as her new shell! Please provide the address of an abode you still own...\n"
+)
 
 	Dest = raw_input("Relative Path to Destination Directory: ")
 	try:
 		DestDirectory = os.listdir(Dest)
 	except:
-		print "\nAn angry octopus has dragged your destination to the bottom of the sea! Such misfortune!\n"
-		sys.exit()
+		sys.exit("\nAn angry octopus has dragged your destination to the bottom of the sea! Such misfortune!\n")
 
 	print "\nChoose Your Illegal Experiment!\n"
 	print "Available Procedures: \n"
@@ -42,16 +43,35 @@ def main():
 		subprotocol = raw_input("Enter 'Q' to query directory, 'D' to calculate dominant colors: ")
 		if subprotocol == 'Q':
 			queryImage = raw_input("Enter relative path to your query image: ")
+			try:
+				os.path.isfile(queryImage)
+			except:
+				sys.exit("\nYour image couldn't make it through the portal in time..\n")
+			if imghdr.what(queryImage) is None:
+				sys.exit("\nYour image has met a gruesome fate in a black hole.\n")
+
 			print "\nColor query protocol activated...\n"
 			colorCode.queryByColor(Home, HomeDirectory, queryImage, Dest)
 		elif subprotocol == 'D':
 			print "\nDominant colors protocol activated...\n"
 			colorCode.dominantColors(Home, HomeDirectory, Dest)
 		else:
-			print "\nSorry, that option was blasted into smithereens with most of the dinosaurs...\n"
-			sys.exit()
+			sys.exit("\nSorry, that option was blasted into smithereens with most of the dinosaurs...\n")
 	elif Protocol == 'D':
-		print "Activating clone detection algorithm....\n"
+		print "\nActivating clone detection algorithm....\n"
+		cloneFate = raw_input("Do you want to move or purge similar images? ('M' - relocate, 'P' - purge): ")
+		if cloneFate != 'M' and cloneFate != 'P': 
+			sys.exit("\nWe don't have the funding for that!\n")
+		queryImage = raw_input("Enter relative path to your query image: ")
+		try:
+			os.path.isfile(queryImage)
+		except:
+			sys.exit("\nOops, threw it into the shredder! Your image now lives on as beautiful ribbons.\n")
+		if imghdr.what(queryImage) is None:
+				sys.exit("\nYour image lost a proton and isn't quite itself anymore.\n")
+
+		cloneCrush.cloneCrusher(Home, HomeDirectory, queryImage, Dest, cloneFate)
+
 		#TODO: would probably have to save past histograms
 	elif Protocol == 'F': # detects the presence of sentient beings (humans and cats)
 		showWindow = raw_input("Would you like to see each image in a popup window? ('Y'/'N'): ")
@@ -60,17 +80,14 @@ def main():
 			faceDetection.cascade()
 			faceDetection.detectLife(Home, HomeDirectory, showWindow, Dest)
 		else:
-			print "\nI'm afraid that's not an option...\n"
-			sys.exit()
+			sys.exit("\nI'm afraid that's not an option...\n")
 	elif Protocol == 'S': # reduces the size of photos
 		dimension = raw_input("Enter your impossible-to-attain ideal size: ")
 		if (dimension.isdigit() == False):
-			print "\nYou'll have to be more specific about the settings for the shrink ray.\n"
-			sys.exit()
+			sys.exit("\nYou'll have to be more specific about the settings for the shrink ray.\n")
 		heightOrWeight = raw_input("Do you want them short or skinny? ('H' - height/'W' - width): ")
 		if (heightOrWeight != 'H' and heightOrWeight != 'W'):
-			print "\nWe cannot comprehend your choice.\n"
-			sys.exit()
+			sys.exit("\nWe cannot comprehend your choice.\n")
 		print "\nBusting out the shrink ray...\n"
 		shrinkRay.shrinkImages(Home, HomeDirectory, dimension, heightOrWeight)
 	elif Protocol == 'T': # retrieves and translates text in photos
